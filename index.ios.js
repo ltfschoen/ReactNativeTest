fa77652cfa8fd52e82c49ccde85e51755742206a
Luke Schoen <ltfschoen@gmail.com>
2017-05-21 10:56:11 +1000

    feat: Add View and Text components to search page

--- index.ios.js
+++ index.ios.js
@@ -13,19 +13,42 @@ import {
 } from 'react-native';
 
 export default class ReactNativeTest extends Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View style={styles.containerTop}>
+          <Text style={styles.logo}>
+            Logo
+          </Text>
+          <View style={styles.subContainerTabs}>
+            <Text style={styles.tabSearch}>
+              Search
+            </Text>
+            <Text style={styles.tabManage}>
+              Manage
+            </Text>
+          </View>
+        </View>
+        <View style={styles.containerMiddle}>
+          <View style={styles.subContainerMiddleSearch}>
+            <View style={styles.searchTop}>
+              <Text style={styles.searchTopText}>
+                Search List
+              </Text>
+            </View>
+            <View style={styles.searchBottom}>
+              <Text style={styles.searchBottomText}>
+                Search
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.containerBottom}>
+          <Text style={styles.searchAdvanced}>
+            Advanced Search
+          </Text>
+        </View>
       </View>
     );
   }
@@ -34,19 +57,98 @@ export default class ReactNativeTest extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'column',
+    justifyContent: 'space-between', // flex-end
+    alignItems: 'center', // flex-start
+    backgroundColor: 'white',
   },
-  welcome: {
+  containerTop: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  logo: {
+    flex: 1,
+    flexDirection: 'row',
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 20,
+    backgroundColor: 'red'
   },
-  instructions: {
+  subContainerTabs: {
+    flex: 1,
+    flexDirection: 'row',
+    margin: 20,
+    backgroundColor: 'white'
+  },
+  tabSearch: {
+    flex: 3,
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    backgroundColor: 'steelblue',
+    margin: 3,
+    borderStyle: 'solid',
+    borderWidth: 3,
+    borderRadius: 4,
+    borderColor: 'black'
+  },
+  tabManage: {
+    flex: 3,
+    textAlign: 'center',
+    color: '#333333',
+    backgroundColor: 'white',
+    margin: 3,
+    borderStyle: 'solid',
+    borderWidth: 3,
+    borderRadius: 4,
+    borderColor: 'black'
+  },
+  containerMiddle: {
+    flex: 3,
+    flexDirection: 'row',
+  },
+  subContainerMiddleSearch: {
+    flex: 1,
+    flexDirection: 'column',
+    margin: 20,
+    borderStyle: 'solid',
+    borderWidth: 3,
+    borderRadius: 4,
+    borderColor: 'black'
+  },
+  searchTop: {
+    flex: 3,
+    flexDirection: 'row'
+  },
+  searchBottom: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  searchTopText: {
+    flex: 3,
+    textAlign: 'center',
+    alignItems: 'center',
+    color: '#333333',
+    backgroundColor: 'steelblue',
+    margin: 10
+  },
+  searchBottomText: {
+    flex: 1,
+    textAlign: 'center',
+    alignItems: 'center',
+    color: '#333333',
+    backgroundColor: 'white',
+    margin: 10
+  },
+  containerBottom: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  searchAdvanced: {
+    flex: 1,
+    textAlign: 'center',
+    color: '#333333',
+    backgroundColor: 'yellow',
+    margin: 20
   },
 });
 
